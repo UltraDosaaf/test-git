9671ec5cdfb49d0966305c42f6d2f236f923f89d
UltraDosaaf <ddsfasdga@mail.ru>
2017-09-11 10:51:08 +0300

    Minor bugfixes

--- click.js
+++ click.js
@@ -1,6 +1,6 @@
 this.btn.addEventListener('click', click.bind(this));
 function click(e){
-	if(e.nativeEvent.button == 0){
-		// console.log('click');
-	}
+   if(e.nativeEvent.button == 0){
+      // console.log('click');
+   }
 }
